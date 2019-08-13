c4844d11974bd82f011ff92dfed13bcfa747d294
Manuel Loza <hector19865@hotmail.com>
2019-08-13 09:17:19 -0500

    Se creo el controlador y sus funciones de Empleado

--- orm/src/main/java/com/soma/sgc/controller/ControllerEmpleado.java
+++ orm/src/main/java/com/soma/sgc/controller/ControllerEmpleado.java
@@ -5,15 +5,15 @@
  */
 package com.soma.sgc.controller;
 
+import com.soma.sgc.model.CatalogoPuestos;
 import com.soma.sgc.model.CatalogoTaller;
 import java.net.UnknownHostException;
 import com.soma.sgc.model.Empleado;
-import com.soma.sgc.model.Usuario;
+import com.soma.sgc.service.CatalogoPuestosService;
 import com.soma.sgc.service.CatalogoTallerService;
 import com.soma.sgc.service.EmpleadoService;
-
-import com.soma.sgc.service.UsuarioService;
-import java.net.InetAddress;
+import static java.time.Instant.now;
+import java.util.Date;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -45,18 +45,22 @@ public class ControllerEmpleado {
     CatalogoTallerService tallerService;
     @Autowired
     EmpleadoService empleadoService;
+    @Autowired
+    CatalogoPuestosService puestosService;
 
     @RequestMapping(value = {"/empleado"}, method = RequestMethod.GET)
     public String usuario(ModelMap model) {
         model.addAttribute("user", usuarioEnSesion());
         if (!estaUsuarioAnonimo()) {
             List<Empleado> lEmpleado = empleadoService.showEmpleado();
+            List<CatalogoTaller> lTaller = tallerService.showTaller();
+            List<CatalogoPuestos> lPuesto = puestosService.showPuesto();
+
             // enviar los datos JSP
             model.addAttribute("lEmpleado", lEmpleado);
-                List<CatalogoTaller> ltaller = tallerService.showTaller();
-            // enviar los datos JSP
-            model.addAttribute("ltaller", ltaller);
-        
+            model.addAttribute("lTaller", lTaller);
+            model.addAttribute("lPuesto", lPuesto);
+
             return "empleado";
         }
         return "login";
@@ -75,6 +79,136 @@ public class ControllerEmpleado {
             return nicknamePrincipal = principal.toString();
         }
     }
+
+    /**
+     * Metodo para agregar Empleados
+     *
+     * @param datos
+     * @return
+     */
+    @RequestMapping(value = "/empleado/agregarEmpleado", method = RequestMethod.POST)
+    public @ResponseBody
+    String agregarEmpleado(@RequestParam(value = "datos[]") String datos[]) {
+        if (!estaUsuarioAnonimo()) {
+            for (String dato : datos) {
+                if (dato.equals("")) {
+                    // si hay datos vacios
+                    return "errorDato";
+                }
+            }
+
+            Date fecha = new Date();
+            
+            CatalogoPuestos lPuesto = puestosService.mostrarNombre(Integer.parseInt(datos[4]));
+            CatalogoTaller lTaller = tallerService.mostrarNombre(Integer.parseInt(datos[6]));
+            //Creacion del objeto 
+            Empleado empleado = new Empleado();
+            empleado.setNombre(datos[0]);
+            empleado.setApp(datos[1]);
+            empleado.setApm(datos[2]);
+            empleado.setEstatus(datos[3]);
+            empleado.setIdpuesto(lPuesto);
+            empleado.setSerie(datos[5]);
+            empleado.setIdtaller(lTaller);
+            empleado.setDescripcion(datos[7]);
+            empleado.setFechaestatus(fecha);
+
+            if (empleadoService.save(empleado)) {
+                return "exito";
+            } else {
+                return "error";
+            }
+        }
+        return "errorAcceso";
+    }
+
+    /**
+     * Metodo que actualiza un empleado
+     *
+     * @param datos
+     * @return
+     */
+    @RequestMapping(value = "/empleado/actualizarEmpleado", method = RequestMethod.POST)
+    public @ResponseBody
+    String actualizarEmpleado(@RequestParam(value = "datos[]") String datos[]) {
+        if (!estaUsuarioAnonimo()) {
+            for (String dato : datos) {
+                if (dato.equals("")) {
+                    // si hay datos vacios
+                    return "errorDato";
+                }
+            }
+
+            List<Empleado> lEmpleado = empleadoService.showEmpleado();
+            if (!lEmpleado.isEmpty()) {
+                for (Empleado empleado : lEmpleado) {
+                    if (empleado.getIdempleado() == Integer.parseInt(datos[8])) {
+                        
+                        Date fecha = new Date();
+
+                        CatalogoPuestos lPuesto = puestosService.mostrarNombre(Integer.parseInt(datos[4]));
+                        CatalogoTaller lTaller = tallerService.mostrarNombre(Integer.parseInt(datos[6]));
+                        //Se llena el modelo
+                        empleado.setNombre(datos[0]);
+                        empleado.setApp(datos[1]);
+                        empleado.setApm(datos[2]);
+                        empleado.setEstatus(datos[3]);
+                        empleado.setIdpuesto(lPuesto);
+                        empleado.setSerie(datos[5]);
+                        empleado.setIdtaller(lTaller);
+                        empleado.setDescripcion(datos[7]);
+                        empleado.setFechaestatus(fecha);
+
+                        if (empleadoService.save(empleado)) {
+                            return "exito";
+                        } else {
+                            return "error";
+                        }
+                    }
+                }
+            }
+        }
+        return "errorAcceso";
+    }
+
+    /**
+     * Metodo para eliminar un empleado
+     * @param datos
+     * @return 
+     */
+    @RequestMapping(value = "/empleado/eliminarEmpleado", method = RequestMethod.POST)
+    public @ResponseBody
+    String eliminarUsuario(@RequestParam(value = "datos[]") String datos[]) {
+        if (!estaUsuarioAnonimo()) {
+            for (String dato : datos) {
+                if (dato.equals("")) {
+                    // si hay datos vacios
+                    return "errorDato";
+                }
+            }//termina de recorrer el arreglo
+
+            List<Empleado> lEmpleado = empleadoService.showEmpleado();
+
+            if (!lEmpleado.isEmpty()) {
+                for (Empleado empleado : lEmpleado) {
+                    int IdEmpleado = Integer.parseInt(datos[0]);
+                    if (empleado.getIdempleado() == IdEmpleado) {
+                        if (empleadoService.delete(IdEmpleado)) {
+                            return "exito";
+                        } else {
+                            return "error";
+                        }
+                    }
+
+                }
+            }
+
+        }//el usuario es anonimo
+
+        return "errorAcceso";
+
+    }
+    
 
     /**
      * Este metodo verificara que un usuario este autenticado correctamente
